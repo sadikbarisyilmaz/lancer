5d863abd1ff3006196136011ceed2db67e0abeda
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-12-30 23:10:41 +0300

    vercel fix

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,13 @@
 import readUserSession from "../actions";
 import { redirect } from "next/navigation";
 export default async function Index() {
-  const {
-    data: { session },
-  } = await readUserSession();
+  // const {
+  //   data: { session },
+  // } = await readUserSession();
 
-  if (!session) {
-    return redirect("/login");
-  }
+  // if (!session) {
+  //   return redirect("/login");
+  // }
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">Home</div>
