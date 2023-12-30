b222835cf3d4e9649c446bb89d747ef13b23e172
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-12-30 23:08:13 +0300

    vercel fix

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,20 +1,10 @@
 import { Navbar } from "@/components/Navbar";
-import readUserSession from "../actions";
-import { redirect } from "next/navigation";
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const {
-    data: { session },
-  } = await readUserSession();
-
-  if (!session) {
-    return redirect("/login");
-  }
-
   return (
     <>
       <Navbar />
