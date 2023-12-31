ee7e9ac813cebdf21a78e9a391fb499f812e2d84
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-12-31 15:32:23 +0300

    page protection vercel

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -1,7 +1,8 @@
 "use server";
 import createSupabaseServerClient from "@/lib/supabase/server";
-
+import { unstable_noStore as noStore } from "next/cache";
 export default async function readUserSession() {
+  noStore();
   const supabase = await createSupabaseServerClient();
   return supabase.auth.getSession();
 }
