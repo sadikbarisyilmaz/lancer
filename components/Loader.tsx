5acb9b427fa57d0e255132fe85fa1e83049a5097
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-01-03 20:22:57 +0300

    loader added to clients page

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,6 +1,6 @@
 export const Loader = () => {
   return (
-    <div className="w-full flex justify-center">
+    <div className="w-full h-screen items-center flex justify-center">
       <div className="animate-ping w-16 h-16 rounded-full loader"></div>
     </div>
   );
