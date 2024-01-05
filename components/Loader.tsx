6cabedaa9625306068904dc07c54a281a8209fb9
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-01-05 16:45:11 +0300

    style fix

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -9,7 +9,7 @@ export const Loader = () => {
           </div>
         </div>
       </div> */}
-      <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-800" />
+      <div className="border-gray-600 h-10 w-10 animate-spin  rounded-full border-8 border-t-black" />
     </div>
   );
 };
