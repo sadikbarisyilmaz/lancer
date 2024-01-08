eae562fc7a19109f1c0c4f886227334c7ecac55a
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-01-08 18:16:15 +0300

    fixed day order

--- components/UpcomingTable.tsx
+++ components/UpcomingTable.tsx
@@ -6,7 +6,7 @@ export const UpcomingTable = () => {
   return (
     <div className="flex justify-center md:p-6 p-4 w-full h-full ">
       <div className="grid sm:grid-cols-2 md:grid-cols-7 w-full ">
-        <div className="p-2 md:border-r border-foreground/10">
+        <div className="p-2 order-1 md:border-r border-foreground/10">
           <h4 className="text-center border-b border-foreground/10 pb-4">
             Today
           </h4>
@@ -14,7 +14,7 @@ export const UpcomingTable = () => {
             <TaskCard />
           </div>
         </div>
-        <div className="p-2 md:border-r border-foreground/10">
+        <div className="p-2 order-2 md:border-r border-foreground/10">
           <h4 className="text-center border-b border-foreground/10 pb-4">
             {format(today.setDate(today.getDate() + 1), "EEEE")}
           </h4>
@@ -23,15 +23,15 @@ export const UpcomingTable = () => {
             <TaskCard />
           </div>
         </div>
-        <div className="p-2 md:border-r border-foreground/10">
+        <div className="p-2 order-3 md:border-r border-foreground/10">
           <h4 className="text-center border-b border-foreground/10 pb-4">
-            {format(today.setDate(today.getDate() + 2), "EEEE")}
+            {format(today.setDate(today.getDate() + 1), "EEEE")}
           </h4>
           <div className="py-2 grid gap-2"></div>
         </div>
-        <div className="p-2 md:border-r border-foreground/10">
+        <div className="p-2 order-4 md:border-r border-foreground/10">
           <h4 className="text-center border-b border-foreground/10 pb-4">
-            {format(today.setDate(today.getDate() + 3), "EEEE")}
+            {format(today.setDate(today.getDate() + 1), "EEEE")}
           </h4>
           <div className="py-2 grid gap-2">
             <TaskCard />
@@ -39,26 +39,26 @@ export const UpcomingTable = () => {
             <TaskCard />
           </div>
         </div>
-        <div className="p-2 md:border-r border-foreground/10">
+        <div className="p-2 order-5 md:border-r border-foreground/10">
           <h4 className="text-center border-b border-foreground/10 pb-4">
-            {format(today.setDate(today.getDate() + 4), "EEEE")}
+            {format(today.setDate(today.getDate() + 1), "EEEE")}
           </h4>
           <div className="py-2 grid gap-2">
             <TaskCard />
             <TaskCard />
           </div>
         </div>
-        <div className="p-2 md:border-r border-foreground/10">
+        <div className="p-2 order-6 md:border-r border-foreground/10">
           <h4 className="text-center border-b border-foreground/10 pb-4">
-            {format(today.setDate(today.getDate() + 5), "EEEE")}
+            {format(today.setDate(today.getDate() + 1), "EEEE")}
           </h4>
           <div className="py-2 grid gap-2">
             <TaskCard />
           </div>
         </div>
-        <div className="p-2 ">
+        <div className="p-2 order-7 ">
           <h4 className="text-center border-b border-foreground/10 pb-4">
-            {format(today.setDate(today.getDate() + 6), "EEEE")}
+            {format(today.setDate(today.getDate() + 1), "EEEE")}
           </h4>
           <div className="py-2 grid gap-2">
             <TaskCard />
