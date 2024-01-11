a479786f288de50a3406970f572a688b090fc70d
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-01-11 23:09:21 +0300

    style fixes

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -7,14 +7,14 @@ interface BannerProps {
 
 export const Banner = ({ title }: BannerProps) => {
   return (
-    <div className="w-full text-4xl md:text-6xl py-4 px-6 flex flex-col border-b border-b-foreground/10 justify-end md:justify-between">
-      <div className="h-full hidden md:flex justify-between md:min-h-[100px]">
+    <div className="w-full text-4xl lg:text-6xl py-4 px-6 flex flex-col border-b border-b-foreground/10 justify-end lg:justify-between">
+      <div className="h-full hidden lg:flex justify-between lg:min-h-[100px]">
         <h1 className="self-end">{title}</h1>
-        <div className="h-full hidden md:flex justify-between">
+        <div className="h-full hidden lg:flex justify-between">
           <ModeToggle />
         </div>
       </div>
-      <div className="h-full md:hidden flex">
+      <div className="h-full lg:hidden flex">
         <h1 className="self-center">{title}</h1>
       </div>
     </div>
