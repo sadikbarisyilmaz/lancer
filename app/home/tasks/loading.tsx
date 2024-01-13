2f9695e49046c24c666eee77664f75dd1d6feba9
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-01-13 19:51:58 +0300

    style fixes

--- app/home/tasks/loading.tsx
+++ app/home/tasks/loading.tsx
@@ -4,9 +4,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <div className="h-full w-full flex lg:max-w-7xl flex-col">
-      <Banner title="Clients" />
-      <div className="p-6">
+    <div className="h-full w-full flex  flex-col ">
+      <Banner title="Tasks" />
+      <div className="p-6 xl:pt-28 xl:self-center xl:w-[1336px] xl:scale-125 flex lg:max-w-7xl flex-col">
         <div className="dark:bg-[#2424247c] bg-[#ffffffcb] rounded-md lg:w-full h-fit  flex flex-col  ">
           <div className="border-b dark:bg-[#2424247c] bg-[#ffffffcb] border-background">
             <Skeleton className="h-[48px]" />
