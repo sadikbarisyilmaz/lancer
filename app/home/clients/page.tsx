8449f88671028f4e0877e7a2303bd3685678695f
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-01-25 20:46:26 +0300

    style fixes

--- app/home/clients/page.tsx
+++ app/home/clients/page.tsx
@@ -11,7 +11,7 @@ export default async function Page() {
     return <Loading />;
   }
   return (
-    <div>
+    <div className="h-full flex flex-col overflow-y-scroll">
       <Banner title="Clients" />
       <div className="p-6 lg:w-full flex flex-col gap-1 animate-fadeIn">
         <div className="xl:max-w-7xl xl:pt-24 flex flex-col md:self-center md:w-full xl:scale-125">
--- app/home/tasks/page.tsx
+++ app/home/tasks/page.tsx
@@ -10,7 +10,7 @@ export default async function Page() {
     return <Loading />;
   }
   return (
-    <div>
+    <div className="h-full flex flex-col overflow-y-scroll">
       <Banner title="Tasks" />
       <div className="p-6 lg:w-full flex flex-col gap-1 animate-fadeIn">
         <div className="xl:max-w-7xl xl:pt-20 flex flex-col md:self-center md:w-full xl:scale-125">
