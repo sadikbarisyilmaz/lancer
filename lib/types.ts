65ee4c3e8e244c361e0b20904a0ad176b59a0760
sadikbarisyilmaz <89347761+sadikbarisyilmaz@users.noreply.github.com>
2025-10-07 20:49:53 +0300

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -4,7 +4,14 @@ export interface User {
   full_name: string;
   avatar_url?: string;
   hashedPassword: string;
+
+  // Relations
+  clients: Client[];
+  tasks: Task[];
+  weeklies: Weekly[];
+  clientNotes: ClientNote[];
 }
+
 export interface Client {
   id: string;
   created_at: Date;
@@ -13,33 +20,18 @@ export interface Client {
   type: string;
   email: string;
   phone: number;
+
+  // Relations
+  user: User;
+  tasks: Task[];
+  notes: ClientNote[];
+  weeklies: Weekly[];
 }
-export interface ClientNote {
-  id: string;
-  created_at: Date | string;
-  user_id: number;
-  content: string;
-}
-export interface ClientFormData {
-  name: string;
-  type: string;
-  email: string;
-  phone: string;
-}
-export interface EditClientFormData {
-  name: string;
-  type: string;
-  email: string;
-  phone: string;
-}
-export interface EditUserFormData {
-  full_name: string;
-  email: string;
-}
+
 export interface Task {
   id: string;
   created_at: Date;
-  set_date: Date | string;
+  set_date: Date;
   set_time: string;
   user_id: string;
   client_id: string;
@@ -48,7 +40,13 @@ export interface Task {
   fee: number;
   payment_status: string;
   frequency: string;
+
+  // Relations
+  user: User;
+  client: Client; // singular
+  notes: TaskNote[];
 }
+
 export interface Weekly {
   id: string;
   created_at: Date;
@@ -60,28 +58,30 @@ export interface Weekly {
   about: string;
   fee: number;
   payment_status: string;
+
+  // Relations
+  user: User;
+  client: Client; // singular
+}
+
+export interface ClientNote {
+  id: string;
+  created_at: Date;
+  user_id: string;
+  content: string;
+  client_id?: string; // optional
+
+  // Relations
+  user: User;
+  client?: Client; // optional
 }
 
 export interface TaskNote {
   id: string;
-  created_at: Date | string;
-  task_id: number;
+  created_at: Date;
+  task_id: string;
   content: string;
-}
-export interface TaskFormData {
-  set_date: Date;
-  set_time: string;
-  title: string;
-  about: string;
-  fee: string;
-  client_id: string;
-  frequency: string;
-}
-export interface EditTaskFormData {
-  set_date: Date | string;
-  set_time: string;
-  title: string;
-  about: string;
-  fee: string;
-  frequency: string;
+
+  // Relations
+  task: Task;
 }
